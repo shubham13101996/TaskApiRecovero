d514403e0bcce695fe7dc80b21856c745cf211d8
shubhampathak <shubham.pathak926@gmail.com>
2023-10-02 11:03:50 +0530

    task api with routes and action

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
+
+app.use("/api/todos", require("./routes/todoRoutes"));
+
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
